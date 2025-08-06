892ff6493335116e45e50a3abb79068d10bb890f
James McCreight <jamesmcc@ucar.edu>
2025-08-06 14:37:25 -0600

    checking final output matches for for numba and numpy; looping on space; refactor of how inputs are passed

--- toy_model.py
+++ toy_model.py
@@ -20,19 +20,19 @@ class Input:
             ]
             self.calc = jitclass(self.Calc, jit_class_spec)(data=data)
         else:
-            raise ValueError(f"Invalid value for calc_method: {calc_method}.")
+            raise ValueError(f"Invalid value: {calc_method=}.")
         return
 
     class Calc:
         def __init__(self, data: np.ndarray) -> None:
             self.data = data
             self.current_index = np.int64(-1)
-            self.current_values = np.nan * self.data[:, 0]
+            self.current_values = np.nan * self.data[0, :]
             return
 
         def advance(self):
-            self.current_index += 1
-            self.current_values[:] = self.data[:, self.current_index]
+            self.current_index += np.int64(1)
+            self.current_values[:] = self.data[self.current_index,]
             return
 
 
@@ -58,6 +58,7 @@ class Upper:
                 ("forcing_0", float64[:]),
                 ("flow", float64[:]),
                 ("flow_previous", float64[:]),
+                ("n_loc", int64),
             ]
             self.calc = jitclass(self.Calc, jit_class_spec)(
                 forcing_0=self.forcing_0,
@@ -65,7 +66,7 @@ class Upper:
                 flow_previous=self.flow_previous,
             )
         else:
-            raise ValueError(f"Invalid value for calc_method: {calc_method}.")
+            raise ValueError(f"Invalid value: {calc_method=}.")
 
     @staticmethod
     def get_inputs() -> tuple:
@@ -82,14 +83,25 @@ class Upper:
             self.forcing_0 = forcing_0
             self.flow = flow
             self.flow_previous = flow_previous
+            self.n_loc = len(self.flow)
             return
 
         def advance(self) -> None:
             self.flow_previous[:] = self.flow
             return
 
-        def calculate(self, dt: np.float64):
-            self.flow[:] = self.flow_previous * 0.95 + self.forcing_0
+        # is parallelization possible?
+        # @njit(
+        #     void(
+        #         self,
+        #         float64[:],  # seg_lateral_inflow
+        #     ),
+        #     fastmath=True,
+        #     parallel=True,
+        # )
+        def calculate(self, dt: np.float64) -> None:
+            for loc in range(self.n_loc):
+                self.flow[loc] = self.flow_previous[loc] * 0.95 + self.forcing_0[loc]
             return
 
 
@@ -115,12 +127,15 @@ class Lower:
                 ("flow", float64[:]),
                 ("storage", float64[:]),
                 ("storage_previous", float64[:]),
+                ("n_loc", int64),
             ]
             self.calc = jitclass(self.Calc, jit_class_spec)(
                 flow=self.flow,
                 storage=self.storage,
                 storage_previous=self.storage_previous,
             )
+        else:
+            raise ValueError(f"Invalid value: {calc_method=}.")
 
     @staticmethod
     def get_inputs() -> tuple:
@@ -140,30 +155,35 @@ class Lower:
             self.flow = flow
             self.storage = storage
             self.storage_previous = storage_previous
+            self.n_loc = len(self.flow)
 
         def advance(self) -> None:
             self.storage_previous[:] = self.storage
             return
 
         def calculate(self, dt: np.float64) -> None:
-            self.storage[:] = self.storage_previous * 0.95 + self.flow * 0.12
+            for loc in range(self.n_loc):
+                self.storage[loc] = (
+                    self.storage_previous[loc] * 0.95 + self.flow[loc] * 0.12
+                )
             return
 
 
 class Model:
-    def __init__(self, process_dict: dict, inputs: dict) -> None:
+    def __init__(self, process_dict: dict) -> None:
         self.process_dict = process_dict
-        self.inputs = inputs
 
         self.n_times = self.get_n_times()
 
         self.model_dict = {}
+        self.inputs_dict = {}
         for kk, vv in process_dict.items():
             init_dict = {kkk: vvv for kkk, vvv in vv.items() if kkk != "class"}
             inputs_req = vv["class"].get_inputs()
             for ii in inputs_req:
-                if ii in inputs.keys():
-                    init_dict[ii] = inputs[ii].calc.current_values
+                if ii in init_dict.keys():
+                    self.inputs_dict[ii] = init_dict[ii]
+                    init_dict[ii] = self.inputs_dict[ii].calc.current_values
                 else:
                     for pp in self.procs_above(kk):
                         if ii in self.model_dict[pp].get_variables():
@@ -175,7 +195,10 @@ class Model:
 
     def get_n_times(self) -> np.int64:
         # would check consistency of this across all inputs
-        return np.int64(list(self.inputs.values())[0].calc.data.shape[1])
+        proc_dict_0 = list(self.process_dict.values())[0]
+        class_0 = proc_dict_0["class"]
+        input_0 = class_0.get_inputs()[0]
+        return np.int64(proc_dict_0[input_0].calc.data.shape[0])
 
     def procs_above(self, proc_name) -> list:
         procs_above = []
@@ -189,7 +212,7 @@ class Model:
         return
 
     def advance(self) -> None:
-        for vv in self.inputs.values():
+        for vv in self.inputs_dict.values():
             vv.calc.advance()
         for vv in self.model_dict.values():
             vv.calc.advance()
@@ -205,7 +228,7 @@ class Model:
             verbose = False
             if verbose:
                 print(f"{tt=}")
-                print(f"{self.inputs['forcing_0'].calc.current_values=}")
+                print(f"{self.inputs_dict['forcing_0'].calc.current_values=}")
                 print(f"{self.model_dict['upper'].calc.forcing_0=}")
                 print(f"{self.model_dict['upper'].calc.flow=}")
                 print(f"{self.model_dict['lower'].calc.flow=}")
@@ -230,23 +253,40 @@ def timer(func):
 
 
 if __name__ == "__main__":
+    # construct input/forcing timeseries
     # do bi-weekly timestep with annual cylce
-    n_times = 500000
-    sin_data = np.sin(np.arange(0, np.pi * 2, np.pi * 2 / n_times))
-    forcing_0 = Input(np.stack((sin_data + 15, sin_data + 25), axis=0))
+    n_time = 5000
+    n_space = 20000
+    sin_data = np.sin(np.arange(0, np.pi * 2, np.pi * 2 / n_time))
+    shifts = np.random.uniform(low=10, high=100, size=n_space)
+    forcing_0_data = np.broadcast_to(
+        sin_data, (n_space, n_time)
+    ).transpose() + np.broadcast_to(shifts, (n_time, n_space))
 
-    input_dict = {"forcing_0": forcing_0}
+    # initial conditions
+    flow_initial_data = np.random.uniform(low=100, high=1000, size=n_space)
+    storage_initial_data = np.random.uniform(low=100, high=500, size=n_space)
 
-    for calc_method in ["numpy", "numba"]:
+    # output arrays, just to be stored to by the first calc_method
+    output_flow = forcing_0_data[0, :] * np.nan
+    output_storage = forcing_0_data[0, :] * np.nan
+
+    calc_methods = ["numpy", "numba"]
+    for calc_method in calc_methods:
+        forcing_0 = Input(forcing_0_data.copy(), calc_method=calc_method)
+        flow_initial = flow_initial_data.copy()
+        storage_initial = storage_initial_data.copy()
+
         process_dict = {
             "upper": {
                 "class": Upper,
-                "flow_initial": np.array([100.0, 200.0]),
+                "forcing_0": forcing_0,
+                "flow_initial": flow_initial,
                 "calc_method": calc_method,
             },
             "lower": {
                 "class": Lower,
-                "storage_initial": np.array([300.0, 700.0]),
+                "storage_initial": storage_initial,
                 "calc_method": calc_method,
             },
         }
@@ -256,13 +296,29 @@ if __name__ == "__main__":
         @timer
         def init_model():
             global model
-            model = Model(process_dict, input_dict)
+            model = Model(process_dict)
 
         @timer
         def run_model():
             global model
             model.run(dt=dt)
 
+        @timer
+        def output_model():
+            global model
+            # check the output at the last timestep
+
+            if calc_method == calc_methods[0]:
+                output_flow[:] = model.model_dict["upper"].flow
+                output_storage[:] = model.model_dict["lower"].storage
+            else:
+                np.testing.assert_equal(output_flow, model.model_dict["upper"].flow)
+                np.testing.assert_equal(
+                    output_storage, model.model_dict["lower"].storage
+                )
+
         print(f"{calc_method=}")
         init_model()
         run_model()
+        output_model()
+        del model
